0a02b32d7c142ade2c56f9c0fe8d352651f7497f
ilcomarian <ilcomarian@gmail.com>
2018-09-11 12:43:46 -0400

    add routes for new project

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_09_10_235131) do
-
-#ActiveRecord::Schema.define(version: 2018_09_11_031300) do
-
+ActiveRecord::Schema.define(version: 2018_09_11_031300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
