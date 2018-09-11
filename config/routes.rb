5c1f53d41fa7ca1dbbf548a064d6c20f8484af43
Mary <l.skyling@gmail.com>
2018-09-11 16:43:28 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  
   resources :aplications
   # resources :reviews
   # project/id/new
+  # proj
   resources :projects do 
     resources :reviews, except: [:show, :index]
+    resources :employers
+
   end
+
   # get 'welcome/index'
   root 'welcome#index'
 
