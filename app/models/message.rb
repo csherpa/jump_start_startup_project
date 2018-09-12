6a9ba5bd99ab1989f885d771c1a4a0e7ddd29edf
Mary <l.skyling@gmail.com>
2018-09-11 23:15:57 -0400

    Chat!!!!111

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,7 +4,20 @@ class Message < ApplicationRecord
 
     validates :employer, presence: true
     validates :developer, presence: true
+    validates :body, presence: true
 
     scope :from_developer, ->(id) { where(developer_id: id) }
     scope :from_employer, ->(id) { where(employer_id: id) }
+
+    def set_user_ids(current_user, recipient_id)
+        if current_user.is_a? Developer
+            self.sender = 'developer'
+            self.developer_id = current_user.id
+            self.employer_id = recipient_id
+        elsif current_user.is_a? Employer
+            self.sender = 'employer'
+            self.employer_id = current_user.id
+            self.developer_id = recipient_id
+        end
+    end
 end
