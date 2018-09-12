b4717748d6a540a8f2e7f3a39bb41cd61465db8d
csherpa <chhoti0712@gmail.com>
2018-09-12 00:57:53 -0400

    installed active storage and migrated

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -2,6 +2,7 @@ class Employer < ApplicationRecord
   has_many :projects
   has_many :developers, through: :projects
   has_many :reviews
+  has_one_attached :image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
