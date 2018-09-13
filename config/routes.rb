eda2723fdc88540b5438b895828c2b5f366fc060
ilcomarian <ilcomarian@gmail.com>
2018-09-13 13:43:40 -0400

    delete acount

--- config/routes.rb
+++ config/routes.rb
@@ -19,9 +19,9 @@ Rails.application.routes.draw do
     get '/chat/:user_id', action: :chat, as: 'chat', on: :collection
   end
 
-
+  devise_for :employers, :controllers => { :registrations => 'employers/registrations' }
   devise_for :developers, path: 'developers', :controllers => { registrations: 'developers/registrations' }
-  devise_for :employers, path: 'employers', :controllers => { registrations: 'employers/registrations' }
+ 
   get '/developers', to: 'developers#index'
   get '/developers/:id', to: 'developers#show', as: 'developer'
   
